28b1773053c9bbbb9db7502786b1f3aa8c94cdc2
Андрей Жиянов <aazhiyanov@gmail.com>
2022-08-16 15:20:01 +0300

    Добавьте файлы проекта.

---
+++ Enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelCalculator
+{
+    public enum Enum
+    {
+        Moskva_Ekaterinburg = 1801,
+        Ekaterinburg_Vladivostok = 7489,
+        Ekaterinburg_Tokio=9164
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FuelCalculator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            const double density = 0.76; 
+            double l = Convert.ToDouble(length.Text);
+            double av = Convert.ToDouble(averageExp.Text);
+
+            double resL = l * av/100;
+            double resKg = resL*density;
+
+            resultKg.Text = resKg.ToString();
+            resultL.Text = resL.ToString();
+
+            
+        }
+    }
+}
